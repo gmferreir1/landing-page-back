bdd4ae2dd436facd723dd1461eb826bf6b7c2f8d
Guilherme Ferreira <gmferreir1@gmail.com>
2019-02-12 11:57:27 -0200

    Finalizando a landing page

---
+++ app/Controllers/MainController.js
@@ -0,0 +1,28 @@
+"use strict";
+const Mail = use("Mail");
+const Promise = use("bluebird");
+
+class MainController {
+  /**
+   * Pega os dados do form
+   * @param {*} param0
+   */
+  async sendFormData({ request, response }) {
+    const requestData = request.all();
+
+    requestData.type_contact = (!requestData.type_contact || !requestData.type_contact.length) ? "nenhuma informação selecionada" : requestData.type_contact.toString();
+    requestData.interest = requestData.interest === "comprar_morar" ? "comprar e morar" : "investir";
+
+    await Mail.send("emails.welcome", requestData, message => {
+      message
+        .to("informatica@masterimoveis.com.br")
+        .from("sigem@masterimoveis.com.br", "Sistema landing page")
+        //.cc("robertojunior@masterimoveis.com.br")
+        .subject("Contato landingpage Alphaville");
+    });
+
+    return response.status(200).json("email send");
+  }
+}
+
+module.exports = MainController;
